2298813e9efe3f8a5fe56a867f7693aeadbdf8fd
zensajnani <zen@sajnani.com>
2020-04-23 12:44:48 +0530

    Experimenting with test_branch_3

--- sentiment.py
+++ sentiment.py
@@ -7,7 +7,18 @@ from secrets import *
 #Authorisation
 try:
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(access_token, access_ttyjtyjreyghnrsth
+    ergerbge
+    fb
+    fdb
+    v
+    dfb
+    gb
+    
+    gvb
+    gb
+    gf
+    btroken_secret)
     api = tweepy.API(auth)
 except:
     print("Error: Authentication Failed")
@@ -16,23 +27,22 @@ except:
 class TwitterSentiment(object):
 
     def timeline_tweets(self):
-        public_tweets = api.home_timeline()
-        for tweet in public_tweets:
+        thr)
+        for tweryn
+        rgnrdgnrygnryet in public_tweets:
             print(tweet.text)
 
     def twitter_user(self):
         user = api.get_user('Zen_Sajnani')
-        print(user)
-
-    def search_hashtag(self, hashtag):
-        number_of_tweets = 5
-        for tweet in tweepy.Cursor(api.search,q="#{}".format(hashtag), lang="en").items(number_of_tweets):
-            if tweet.retweeted:
-                print("Retweeted")
-                number_of_tweets += 1
-            else:
-                print("Not Retweeted")
-                print (tweet.text)
-
+        print(user)rth
 ts = TwitterSentiment()
 
+ejnfvjbe rjhfbvjhbe tfb textrth
+rt
+s
+fgb
+sfgb
+rt
+j
+5y
+jtyj
