c0f7a78c533c7cac156502b5313bb87e2847494a
zensajnani <zen@sajnani.com>
2020-04-23 12:36:26 +0530

    Version 1.0.1

--- sentiment.py
+++ sentiment.py
@@ -2,18 +2,17 @@ import re
 import tweepy 
 import pandas as pd
 import numpy as np
-from tweepy import OAuthHandler, Stream
-from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
 from textblob import TextBlob 
 from secrets import *
 
 class TwitterSentiment():
     
     #Tweet Query
-    query = "Tesla"
+    query = "Lockdown"
 
     #Tweet Count
-    count = 100
+    count = 40
 
     #Authorisation
     try:
@@ -23,13 +22,16 @@ class TwitterSentiment():
     except:
         print("Error: Authentication Failed")
 
+    #Get Tweets
     def get_tweets(self):
         fetched_tweets = self.api.search(q = self.query, count = self.count, lang="en")
         return fetched_tweets
 
+    #Clean the tweets by removing special characters and links
     def clean_tweet(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
     
+    #Classify sentiment of tweet using TextBlob
     def tweet_sentiment(self, tweet):
         analysis = TextBlob(self.clean_tweet(tweet))
         if analysis.sentiment.polarity > 0: 
@@ -39,6 +41,7 @@ class TwitterSentiment():
         else: 
             return 'negative'
 
+    #Create Pandas dataframe to store tweets and sentiment
     def create_data_frame(self, tweets):
         df = pd.DataFrame()
         df['Tweets'] = np.array([tweet.text for tweet in tweets])
@@ -51,4 +54,4 @@ if __name__ == "__main__":
     ts.create_data_frame(tweets)
     df = ts.create_data_frame(tweets)
     df['Sentiment'] = np.array([ts.tweet_sentiment(tweet) for tweet in df['Tweets']])
-    print(df.head(30))
+    print(df.head(ts.count))
